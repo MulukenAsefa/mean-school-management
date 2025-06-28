51ee2039eec0a9e9060e0cc30e738b033c28b84f
Muluken Assefa <mulukenasefateju@gmail.com>
2025-06-28 13:17:23 +0200

    newnew

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://school-mgmt-backend.onrender.com/api' // <-- Paste your live URL here
+  apiUrl: 'https://mean-school-management.onrender.com/api'
 };
